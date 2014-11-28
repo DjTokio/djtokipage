ea263556a84f498e987d573953fd512ab6f063c4
Yahir Axel Keymurth <arkaner@outlook.com>
2014-11-28 07:21:37 -0600

    Se agrega el index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>DJ Tokio | INICIO</title>
+	<link rel="stylesheet" href="" />
+	<script type="text/javascript" src=""></script>
+</head>
+<body>
+	
+</body>
+</html>
