66ba9eaec9853fd05d3c136d0cb6a33a5a71e7cf
Yahir Axel Keymurth <arkaner@outlook.com>
2014-11-28 08:49:33 -0600

    Se agregan los archivos de estilo y de js

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title>DJ Tokio | INICIO</title>
-	<link rel="stylesheet" href="" />
-	<script type="text/javascript" src=""></script>
+	<link rel="stylesheet" href="css/stylus.css" />
+	<script type="text/javascript" src="js/js.js"></script>
 </head>
 <body>
 	
